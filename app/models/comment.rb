0b19a4fdc01f6fd9a25ccd555158d2e62e896ec5
theIanMilan <theIanMilan@users.noreply.github.com>
2021-07-21 20:11:40 +0800

    Add image processing gem; Adjust comment styling

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   include Visible
   belongs_to :article
 
-  validates :commenter, presence: true
+  validates :commenter, presence: true, length: {maximum: 20}
   validates :body, presence: true, length: {maximum: 250}
 end
